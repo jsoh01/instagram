a469ae6d7ff0353f0185ab8ab7d24c2245fa72e8
jsoh01 <jsoh3747@gmail.com>
2023-10-24 15:54:48 +0900

    initialized

--- app/components/Feed.jsx
+++ app/components/Feed.jsx
@@ -4,20 +4,41 @@ import { DM } from "../icons/DM";
 import { Heart } from "../icons/Heart";
 import { Menu } from "../icons/Menu";
 
-export const Feed = () => {
+/**
+ * User
+ * {
+ *   id: string;
+ *   name: string;
+ *   profileImg: string;
+ * }
+ */
+
+/**
+ * Content
+ * {
+ *   id: string;
+ *   author: User;
+ *   location: string;
+ *   image: string;
+ *   text: string;
+ *   liked: User[]
+ * }
+ */
+
+export const Feed = ({ content }) => {
   return (
-    <div className="w-[400px] bg-white ">
+    <div className="w-[400px] bg-white mb-1">
       <div id="header" className="flex items-center justify-between p-2">
         {/* profile */}
         <div className="flex items-center ">
           <div
-            className="rounded-full w-10 h-10
-           bg-[url('https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png')]
-           bg-contain mr-2"
+            className={`rounded-full w-10 h-10
+            bg-[url('https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png')]
+           bg-contain mr-2`}
           />
           <div>
-            <div className="font-semibold">frogman</div>
-            <div className="font-light">seoul</div>
+            <div className="font-semibold">{content.author.name}</div>
+            <div className="font-light">{content.location}</div>
           </div>
         </div>
         {/* 더보기 버튼 */}
@@ -28,7 +49,7 @@ export const Feed = () => {
       <div id="content" className="w-[400px] h-[400px]">
         <img
           className="object-cover	w-[400px] h-[400px]"
-          src="https://cdn.britannica.com/38/196638-131-7BF02881/Santa-Claus.jpg"
+          src={content.image}
           alt="img"
         />
       </div>
@@ -45,10 +66,10 @@ export const Feed = () => {
       <div id="comments" className="p-2">
         {/* 누가 좋아요했는지 + 좋아요한 사람의 수 */}
         <div>
-          Liked by <b>{40} others</b>
+          Liked by <b>{content.liked.length} others</b>
         </div>
         <div>
-          <b>frogman</b> Christmas will coming soon🤗🤗!!
+          <b>{content.author.name}</b> {content.text}
         </div>
         {/* 내가 작성한 컨텐츠의 글 */}
         {/* 댓글들 */}
