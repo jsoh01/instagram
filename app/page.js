fe3d9833b45544a0575700c77a6c65773450683f
jsoh01 <jsoh3747@gmail.com>
2023-10-24 11:49:20 +0900

    initailized

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      배포를 다시 했습니다. by ablearn
+      배포를 다시 했습니다. 11:49 by ablearn
     </main>
   );
 }
