79cf9ce9eec1be0c605248cb53b645cf2b1c09e4
jsoh01 <jsoh3747@gmail.com>
2023-10-24 14:49:12 +0900

    initialized

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { Feed } from "./components/Feed";
 
 // 새로운 이미지를 올리는 화면
-// feeds 다른 사람들의 피드를 보는 화면
+// feeds 다른사람들의 피드를 보는 화면
 // my 내 정보화면
 
 export default function Home() {
